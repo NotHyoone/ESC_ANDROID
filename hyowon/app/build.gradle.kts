b72b965559fcdc2fd80e939613604a5bf06bc1fa
NotHyoone <gydnjs3505@gmail.com>
2026-04-10 17:57:44 +0900

    4 10 캠프 자료 업데ã이트

--- hyowon/app/build.gradle.kts
+++ hyowon/app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        var kotlinCompilerExtensionVersion = "1.5.8"
     }
     buildFeatures {
         compose = true
@@ -55,4 +56,30 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // 1. Compose BOM (버전 관리용 맵)
+    // 개별 라이브러리의 버전을 일일이 지정하지 않아도 서로 호환되는 버전을 알아서 맞춰줍니다.
+    val composeBom = platform("androidx.compose:compose-bom:2024.02.01")
+    implementation("androidx.compose:compose-bom:2026.03.01")
+    androidTestImplementation("androidx.compose:compose-bom:2026.03.01")
+
+    // 2. 핵심 UI 라이브러리
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview") // Preview 기능
+
+    // 3. Material 3 디자인 (안드로이드 최신 디자인 시스템)
+    implementation("androidx.compose.material3:material3")
+
+    // 4. Activity 연결 (ComponentActivity 등 Compose를 실행하기 위함)
+    implementation("androidx.compose.runtime:runtime") // state 관리 핵심
+    implementation("androidx.activity:activity-compose:1.13.0")
+
+    // 5. 툴링 (디버그 시 필수)
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
+    implementation("androidx.navigation:navigation-compose:2.9.7")
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
 }
